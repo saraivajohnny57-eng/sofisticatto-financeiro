0acb15cb81dcbacd25d5d35b0f181a2bc59b81ae
saraivajohnny57-eng <saraivajohnny57@gmail.com>
2026-08-04 13:14:22 -0300

    Delete api/integracoes directory

--- api/integracoes/validar-chave.js
+++
@@ -1,7 +0,0 @@
-const {json,exigirAdmin}=require("./_utils");
-
-module.exports=async function handler(req,res){
-  if(req.method!=="POST")return json(res,405,{ok:false,erro:"Método não permitido."});
-  if(!exigirAdmin(req,res))return;
-  return json(res,200,{ok:true});
-};
